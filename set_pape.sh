ce5f5f23034f62f560bff55523070f8e7d3946d0
wmamul <wojciech.mamul@gmail.com>
2021-09-02 14:35:55 +0200

    startup fix

--- set_pape.sh
+++ set_pape.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
-PAPES=($(ls $WALLS_PATH | awk '{print $9}'))
+WALLS_PATH="$HOME/Pictures/Wallpapers" 
+PAPES=($(ls $WALLS_PATH | tr -s ' ' | cut -f 9 -d ' '))
 PAPES_COUNT=${#PAPES[*]}
-RANDOM_NUMBER=$(shuf -i 0-$(expr $PAPES_COUNT - 1) -n 1)
+PAPES_LAST_INDEX=$(expr $PAPES_COUNT - 1)
+RANDOM_NUMBER=$(shuf -i 0-$PAPES_LAST_INDEX -n 1)
 
 DISPLAY=:0 feh --bg-fill $WALLS_PATH/${PAPES[$RANDOM_NUMBER]}
